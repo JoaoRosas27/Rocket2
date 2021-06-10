170a0f4d5a42bd67103e1d14ae96323da5b29a95
JoaoRosas27 <81985345+JoaoRosas27@users.noreply.github.com>
2021-06-10 11:26:49 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,3 +15,11 @@ let keyF, keyR, keyLEFT, keyRIGHT;
 
 // changed the theme and added a timer. 
 // Tried to change the title but for some reason it doesn't update even though its coded to Target Practice
+
+// point breakdown
+// Tracks high score - 5
+// Implemented new background - 5
+// Display time remaining - 10
+// Create new animated sprite for the spaceship - 10
+// Create new art for all of the in game assests - 20
+// Redesign the game - 60
